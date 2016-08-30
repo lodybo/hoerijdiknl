07f2546ea26a9818443f93f0e9cf56fb8047385d
Lody Borgers <hi@lodybo.nl>
2016-08-30 11:23:23 +0200

    Added and configured Angular Routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,18 @@
-angular.module("hoerijdiknlApp", ["firebase"]);
+angular.module("hoerijdiknlApp", ["firebase", "ngRoute"])
+    .config(function($routeProvider, $locationProvider) {
+        $routeProvider
+        .when("/", {
+            templateUrl: "views/ListView.html",
+            controller: "ListCtrl"
+        })
+        .when('/profile/', {
+            templateUrl: 'views/ProfileView.html',
+            controller: 'ProfileCtrl'
+        })
+        .otherwise({
+            redirectTo: "/"
+        });
+
+        // configure html5 to get links working on jsfiddle
+        $locationProvider.html5Mode(true);
+    });
